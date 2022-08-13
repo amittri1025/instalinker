62396e925bfd8c2be601e116687ff2d97a87a412
Amit Tripathi <tripathiamit2021@gmail.com>
2022-08-14 02:00:34 +0530

    Initialed Repo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h2 className=''>Hello WOrld</h2>
+    <h2 className='w-full h-36 bg-blue-500 text-center flex justify-center items-center'>Insta Project</h2>
   )
 }
 
